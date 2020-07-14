6e1f2719e662806a6a0090076da38b45c9b2a294
Varun Vasan V <varun@pop-os.localdomain>
2020-07-14 14:07:41 +0530

    style change

---
+++ index.js
@@ -0,0 +1,36 @@
+(function () {
+    window.onload = function () {
+        const steps = document.getElementsByClassName('step');
+        let front = 0;
+        let bottom = 1;
+        let back = 2;
+        let ticking = false;
+        function prev(i) {
+            if (i == 0) {
+                return 2;
+            }
+            return (i - 1) % 3;
+        }
+        function changeClass() {
+            steps[front].classList.remove('front');
+            steps[bottom].classList.remove('bottom');
+            steps[back].classList.remove('back');
+            front = prev(front);
+            bottom = prev(bottom);
+            back = prev(back);
+            steps[front].classList.add('front');
+            steps[bottom].classList.add('bottom');
+            steps[back].classList.add('back');
+        }
+        function cycleClass() {
+            if (!ticking) {
+                window.requestAnimationFrame(function () {
+                    changeClass();
+                    ticking = false;
+                    setTimeout(cycleClass, 1500);
+                });
+            }
+        }
+        cycleClass();
+    }
+})()
